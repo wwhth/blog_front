6b11dca07904ef6a6a228432251bc8b1486c0802
zhangyunzhi-web <318866560@qq.com>
2023-05-09 00:58:55 +0800

    chore:ep switch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,53 @@
 <template>
   <div class="home">
-    <div class="step">
-      <div v-for="(item, index) in arr" :key="index">
-        <div :class="{ before: index + 1 <= state }">{{ item.name }}</div>
+    <div class="test">
+      <div
+        class="bg"
+        :style="{ background: switch1 ? '#409eff' : '#f5f5f5' }"
+        @click="switchTap"
+      >
+        <div v-if="!switch1" class="close"></div>
+        <div v-else class="open"></div>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted, reactive, ref } from "vue";
-
-interface StepItem {
-  name: string;
-}
-onMounted(() => {});
-let state: number = Math.ceil(Math.random() * 4);
-const arr: StepItem[] = reactive([
-  {
-    name: "企业充值",
-  },
-  {
-    name: "平台审核",
-  },
-  {
-    name: "充值入账",
-  },
-  {
-    name: "TEST",
-  },
-]);
+import { ref } from "vue";
+// localStorage.setItem("switch1", "false");
+// const localSwitch = Boolean(localStorage.getItem("switch1"));
+// 设置默认的选项
+// let switch1 = ref<boolean>(localSwitch);
+let switch1 = ref<boolean>(false);
+// 点击方法
+const switchTap = (): void => {
+  switch1.value = !switch1.value;
+  // localStorage.setItem("swicth1", switch1.value + "");
+};
 </script>
-<style scoped>
-.step {
+<style scoped lang="scss">
+.bg {
+  width: 80px;
+  height: 30px;
+  border-radius: 15px;
+  position: relative;
   display: flex;
-  justify-content: space-between;
-  width: 300px;
-  height: 200px;
-  margin: auto;
-  background-color: red;
   align-items: center;
-  color: #fe9425;
-}
-.before {
-  color: #35c767;
+  .close {
+    width: 26px;
+    height: 26px;
+    background-color: #ffffff;
+    border-radius: 50%;
+    margin-left: 2px;
+  }
+  .open {
+    width: 26px;
+    height: 26px;
+    background-color: #ffffff;
+    border-radius: 50%;
+    position: absolute;
+    right: 0;
+    margin-right: 2px;
+  }
 }
 </style>
